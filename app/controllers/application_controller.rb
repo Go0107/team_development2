3f80fe63284d36f5d42eb036cef9b6aab8a20238
daisuke-kikukawa <daisuke.kikukawa.16@gmail.com>
2024-03-29 23:07:36 +0900

    不具合修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
   def configure_permitted_parameters
-      #新規登録時のストロングパラメータに「username」の追加
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
